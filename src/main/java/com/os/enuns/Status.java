3bf21f1804f59c31fa071128b5e1823b68a2ff84
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-15 23:26:24 -0300

    Criando os Enuns

---
+++ src/main/java/com/os/enuns/Status.java
@@ -0,0 +1,30 @@
+package com.os.enuns;
+
+public enum Status {
+	ABERTO(0, "ABERTO"), 	ANDAMENTO(1, "ANDAMENTO"),	ENCERRADO(2, "ENCERRADO");
+	
+	private Integer cod;
+	private String descricao;
+	private Status(Integer cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	public Integer getCod() {
+		return cod;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static Status toEnunum(Integer cod) {
+		if(cod == null) {
+			return null;
+		}
+		for(Status x : Status.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("STATUS inválidada! " + cod);
+	}
+}
