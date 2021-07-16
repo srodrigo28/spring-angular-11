3e4d71aae170af91b1fdbbd3bae8af3b79a22b79
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-16 01:32:50 -0300

    Insert Crud Command Runner

--- src/main/java/com/os/OsApplication.java
+++ src/main/java/com/os/OsApplication.java
@@ -1,13 +1,48 @@
 package com.os;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
+
+import com.os.domain.Cliente;
+import com.os.domain.OS;
+import com.os.domain.Tecnico;
+import com.os.enuns.Prioridade;
+import com.os.enuns.Status;
+import com.os.repositories.ClienteRepository;
+import com.os.repositories.OSRepository;
+import com.os.repositories.TecnicoRepository;
 
 @SpringBootApplication
-public class OsApplication {
+public class OsApplication implements CommandLineRunner{
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private OSRepository osRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Tecnico t1 = new Tecnico(null, "Sebastiao Rodrigo", "010.460.381-09", "(88) 98230-4040");
+		Cliente c1 = new Cliente(null, "Aline Nogueira", "010.460.381-09", "(66) 97230-2030");
+		OS os1 = new OS(null, Prioridade.ALTA, "Teste no pc", Status.ABERTO, t1, c1);
+		
+		t1.getList().add(os1);
+		c1.getList().add(os1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(t1));
+		clienteRepository.saveAll(Arrays.asList(c1));
+		osRepository.saveAll(Arrays.asList(os1));
 	}
 
 }
---
+++ src/main/java/com/os/repositories/OSRepository.java
@@ -0,0 +1,11 @@
+package com.os.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.os.domain.OS;
+
+@Repository
+public interface OSRepository extends JpaRepository<OS, Integer>{
+
+}
