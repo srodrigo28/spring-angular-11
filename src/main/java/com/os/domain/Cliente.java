f28b4bca60a7a5e5bcf611a3f3c5a0c8fa0b88d5
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-15 23:04:00 -0300

    Projeto e Domain Criados

---
+++ src/main/java/com/os/domain/Cliente.java
@@ -0,0 +1,12 @@
+package com.os.domain;
+
+public class Cliente extends Pessoa{
+
+	public Cliente() {
+		super();
+	}
+	public Cliente(Integer id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+
+}
---
+++ src/main/java/com/os/domain/Tecnico.java
@@ -0,0 +1,14 @@
+package com.os.domain;
+
+public class Tecnico extends Pessoa {
+
+	public Tecnico() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+		// TODO Auto-generated constructor stub
+	}
+}
